3ddc5948db72855ac7ce04379f1a0e3b47bbe96d
5argon <pyasry@gmail.com>
2022-05-23 10:14:28 +0700

    first

--- sync.ts
+++ sync.ts
@@ -1,14 +1,30 @@
-import { createStory } from "./create-story.ts";
+import { createTreeJson } from "./create-tree-json.ts";
 import { getStoriesRecursive } from "./input-processing.ts";
-import { common, emptyDir } from "./mod.ts";
+import { mirrorStoryFile } from "./mirror-story-file.ts";
+import { emptyDir, ensureFile, join } from "./deps.ts";
 
 export async function sync(
   inputFolder: string,
   outputFolder: string
 ): Promise<void> {
-  console.log(`Detected changes in ${inputFolder}`);
+  console.log(
+    `Regenerating ${outputFolder} according to stories found in ${inputFolder}`
+  );
   const gn = await getStoriesRecursive(inputFolder);
-  await emptyDir(outputFolder)
-  const promises = gn.map((x) => createStory(x, outputFolder));
-  await Promise.all(promises);
+
+  await emptyDir(outputFolder);
+
+  const promises = gn.map((x) => mirrorStoryFile(inputFolder, x, outputFolder));
+  const layoutFilePromise = Deno.writeTextFile(
+    join(outputFolder, "__layout@storybook.svelte"),
+    "<slot />"
+  );
+  const indexFilePromise = ensureFile(join(outputFolder, "index.svelte"));
+  const treeJsonPromise = createTreeJson(inputFolder, gn, outputFolder);
+  await Promise.all([
+    ...promises,
+    layoutFilePromise,
+    indexFilePromise,
+    treeJsonPromise,
+  ]);
 }
